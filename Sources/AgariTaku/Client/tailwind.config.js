f87930b56d49ebd5936e9ebda4f21e75a0416923
Jinbi Jin <jinbijin@jinbijin.nl>
2020-12-20 20:56:34 +0000

    Modify loading screen

--- Sources/AgariTaku/Client/tailwind.config.js
+++ Sources/AgariTaku/Client/tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: colors.emerald,
+      accent: colors.blue,
+      white: '#FFFFFF',
+    },
   },
   variants: {
     extend: {},
