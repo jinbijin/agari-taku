77183d25c662f122d3decb52c614b1c4e49f5db3
Jinbi Jin <jinbijin@jinbijin.nl>
2020-12-27 16:00:15 +0000

    Add shuffler integration tests

---
+++ Sources/AgariTaku.Shared.Tests/ShufflerTests.cs
@@ -0,0 +1,25 @@
+﻿using AgariTaku.Shared.GameState;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AgariTaku.Shared.Tests
+{
+    public class ShufflerTests
+    {
+        [Fact]
+        public void ShufflerShouldPermuteInput()
+        {
+            // Arrange
+            IShuffler shuffler = new Shuffler();
+            List<int> initialList = new() { 1, 2, 3 };
+
+            // Act
+            IReadOnlyCollection<int> shuffledList = shuffler.Shuffle<int>(initialList);
+
+            // Assert
+            shuffledList.Should().OnlyHaveUniqueItems();
+            shuffledList.Should().BeEquivalentTo(initialList);
+        }
+    }
+}
