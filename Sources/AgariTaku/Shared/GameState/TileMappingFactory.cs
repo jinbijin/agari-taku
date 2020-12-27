c059d6b3ade8b1f1c86ca2bd685187713d713a54
Jinbi Jin <jinbijin@jinbijin.nl>
2020-12-27 16:09:26 +0000

    Implement TileMappingFactory

--- Sources/AgariTaku/Shared/GameState/TileMappingFactory.cs
+++ Sources/AgariTaku/Shared/GameState/TileMappingFactory.cs
@@ -4,9 +4,22 @@ namespace AgariTaku.Shared.GameState
 {
     public class TileMappingFactory : ITileMappingFactory
     {
+        private readonly ITileGenerator _generator;
+        private readonly IShuffler _shuffler;
+        private readonly IBreakLocationLogic _breakLogic;
+        private readonly IWallIndexConverter _wallIndexConverter;
+
+        public TileMappingFactory(ITileGenerator generator, IShuffler shuffler, IBreakLocationLogic breakLogic, IWallIndexConverter wallIndexConverter)
+        {
+            _generator = generator;
+            _shuffler = shuffler;
+            _breakLogic = breakLogic;
+            _wallIndexConverter = wallIndexConverter;
+        }
+
         public ITileMapping Create(Player dealer)
         {
-            throw new System.NotImplementedException();
+            return new TileMapping(_generator, _shuffler, _breakLogic, _wallIndexConverter, dealer);
         }
     }
 }
